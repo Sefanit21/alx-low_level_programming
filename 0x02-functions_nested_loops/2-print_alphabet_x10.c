3803e08e6df2e213dcdc5f2983c63c07e9335cac
Sefanit21 <sefanitaraya09@gmail.com>
2023-09-13 09:20:09 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * print_alphabet_x10 - check the code
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= '2'; letter++)
+		{
+			putchar(letter);
+		}
+		putchar("\n");
+	}
+}
